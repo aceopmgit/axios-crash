b97f16959112cce368dd2edeee39f21cac5e6b9f
aceopmgit <ace.opm.sales@gmail.com>
2023-12-06 09:00:36 +0530

    Making SQL easy with Sequelize

--- models/product.js
+++ models/product.js
@@ -1,34 +1,27 @@
-const db=require('../util/database')
+const Sequelize=require('sequelize');
 
-const Cart=require('./cart');
+const sequelize=require('../util/database');
 
-
-module.exports = class Product {
-  constructor(id,title, imageUrl, description, price) {
-    this.id=id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product=sequelize.define('product',{
+  id:{
+    type:Sequelize.INTEGER,
+    autoIncrement:true,
+    allownull:false,
+    primaryKey:true
+  },
+  title:Sequelize.STRING,
+  price:{
+    type:Sequelize.DOUBLE,
+    allownull:false
+  },
+  imageUrl:{
+    type:Sequelize.STRING,
+    allownull:false
+  },
+  description:{
+    type:Sequelize.STRING,
+    allownull:false
   }
+})
 
-  save() {
-    return db.execute(
-      'INSERT INTO products (title,price,imageUrl,description) VALUES(?,?,?,?)',
-      [this.title,this.price,this.imageUrl,this.description]
-    );
-    }
-
-  static deleteById(id){
-     return db.execute('DELETE FROM products WHERE products.id=?',[id])
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products')
-  }
-
-  static findById(id){
-    return db.execute('SELECT * FROM products WHERE products.id=?',[id])
-  }
-
-};
+module.exports=Product;
--- util/database.js
+++ util/database.js
@@ -1,9 +1,10 @@
-const mysql=require('mysql2')
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'Ace@1535'
+const Sequelize=require('sequelize');
+
+const sequelize=new Sequelize('node-complete','root','Ace@1535',{
+    dialect:'mysql',
+    host:'localhost'
 })
 
-module.exports=pool.promise();
+
+
+module.exports=sequelize
